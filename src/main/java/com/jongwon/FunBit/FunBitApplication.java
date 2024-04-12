9afbe51c92351f5de5dec6f91103dc35afbc56b0
jongwon-kr <growjong8802@gmail.com>
2024-04-12 22:41:12 +0900

    start FunBit

---
+++ src/main/java/com/jongwon/FunBit/FunBitApplication.java
@@ -0,0 +1,13 @@
+package com.jongwon.FunBit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunBitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunBitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jongwon/FunBit/FunBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongwon.FunBit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
