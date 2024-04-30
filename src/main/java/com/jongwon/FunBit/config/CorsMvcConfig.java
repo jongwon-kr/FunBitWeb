7aac3601463269630e9da534e8fc5e7319271ba4
jongwon-kr <growjong8802@gmail.com>
2024-04-30 23:59:46 +0900

    Logout 구현

--- src/main/java/com/jongwon/FunBit/config/CorsMvcConfig.java
+++ src/main/java/com/jongwon/FunBit/config/CorsMvcConfig.java
@@ -11,7 +11,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .exposedHeaders("Set-Cookie")
+                .exposedHeaders("Set-Cookie") //s
                 .allowedOrigins("http://localhost:3000");
     }
 }
